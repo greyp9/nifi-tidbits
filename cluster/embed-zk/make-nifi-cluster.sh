1ca64b78de77850d6769a9e714e3623924548633
Paul Grey <greyp9@yahoo.com>
2022-02-20 21:29:38 -0500

    linux compatibility

--- cluster/embed-zk/make-nifi-cluster.sh
+++ cluster/embed-zk/make-nifi-cluster.sh
@@ -4,6 +4,11 @@ REAL_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $REAL_PATH)
 SCRIPT_NAME=$(basename $REAL_PATH)
 
+#echo REAL_PATH=$REAL_PATH
+#echo SCRIPT_DIR=$SCRIPT_DIR
+#echo SCRIPT_NAME=$SCRIPT_NAME
+#echo SHELL=$SHELL
+
 NIFI_ROOT=$1
 if [ "$NIFI_ROOT" = "" ]; then
   echo usage: $SCRIPT_NAME [nifi_root_dir]
@@ -17,11 +22,11 @@ fi
 
 if [ $SHELL = "/bin/sh" ]
 then
-  SAXON="java -jar ~/.m2/repository/net/sf/saxon/Saxon-HE/10.6/Saxon-HE-10.6.jar"
+  SAXON="java -jar $HOME/.m2/repository/net/sf/saxon/Saxon-HE/10.*/Saxon-HE-10.*.jar"
   SED="sed -i"
 elif [ $SHELL = "/bin/bash" ]
 then
-  SAXON="java -jar ~/.m2/repository/net/sf/saxon/Saxon-HE/10.6/Saxon-HE-10.6.jar"
+  SAXON="java -jar $HOME/.m2/repository/net/sf/saxon/Saxon-HE/10.*/Saxon-HE-10.*.jar"
   SED="sed -i"
 elif [ $SHELL = "/bin/zsh" ]
 then
@@ -42,6 +47,11 @@ NIFI_BIN_DIR_GLOB=$NIFI_ROOT"/nifi-assembly/target/nifi*bin"
 NIFI_BIN_DIR=$(echo $NIFI_BIN_DIR_GLOB)
 NIFI_CLUSTER_DIR=$NIFI_BIN_DIR"/cluster"
 
+#echo NIFI_TOOLKIT_DIR=$NIFI_TOOLKIT_DIR
+#echo NIFI_IMAGE_DIR=$NIFI_IMAGE_DIR
+#echo NIFI_BIN_DIR=$NIFI_BIN_DIR
+#echo NIFI_CLUSTER_DIR=$NIFI_CLUSTER_DIR
+
 #cd $NIFI_TOOLKIT_DIR && ./bin/tls-toolkit.sh standalone -n 'nifi[1-3]' -C 'CN=nifi' -c 'ca.nifi' -O
 cd $NIFI_TOOLKIT_DIR && ./bin/tls-toolkit.sh standalone -n 'localhost' -C 'CN=nifi' -c 'ca.nifi' -O
 
@@ -49,22 +59,24 @@ rm -rf $NIFI_CLUSTER_DIR
 mkdir $NIFI_CLUSTER_DIR
 for n in `seq 1 $NIFI_NODES`; do
 
+  echo NIFI_CLUSTER_DIR_IT=$NIFI_CLUSTER_DIR/nifi$n
+
   cp -R $NIFI_IMAGE_DIR $NIFI_CLUSTER_DIR/nifi$n
   # cp -R $NIFI_TOOLKIT_DIR/nifi$n/*.* $NIFI_CLUSTER_DIR/nifi$n/conf
   cp -R $NIFI_TOOLKIT_DIR/localhost/*.* $NIFI_CLUSTER_DIR/nifi$n/conf
 
-  sed -i '' 's/nifi.cluster.flow.election.max.candidates=/nifi.cluster.flow.election.max.candidates='"$NIFI_NODES"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.cluster.flow.election.max.wait.time=5 mins/nifi.cluster.flow.election.max.wait.time=2 min/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.cluster.is.node=false/nifi.cluster.is.node=true/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.cluster.load.balance.host=/nifi.cluster.load.balance.host=localhost/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.cluster.load.balance.port=6342/nifi.cluster.load.balance.port=634${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.cluster.node.address=nifi${n}/nifi.cluster.node.protocol.address=localhost/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.cluster.node.protocol.port=11443/nifi.cluster.node.protocol.port=1144${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.remote.input.socket.port=10443/nifi.remote.input.socket.port=1044${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.sensitive.props.key=/nifi.sensitive.props.key=this-is-my-sensitive-props-key/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.state.management.embedded.zookeeper.start=false/nifi.state.management.embedded.zookeeper.start=true/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.web.https.host=nifi${n}/nifi.web.https.host=localhost/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
-  sed -i '' "s/nifi.web.https.port=9443/nifi.web.https.port=944${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED 's/nifi.cluster.flow.election.max.candidates=/nifi.cluster.flow.election.max.candidates='"$NIFI_NODES"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.cluster.flow.election.max.wait.time=5 mins/nifi.cluster.flow.election.max.wait.time=2 min/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.cluster.is.node=false/nifi.cluster.is.node=true/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.cluster.load.balance.host=/nifi.cluster.load.balance.host=localhost/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.cluster.load.balance.port=6342/nifi.cluster.load.balance.port=634${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.cluster.node.address=nifi${n}/nifi.cluster.node.protocol.address=localhost/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.cluster.node.protocol.port=11443/nifi.cluster.node.protocol.port=1144${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.remote.input.socket.port=10443/nifi.remote.input.socket.port=1044${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.sensitive.props.key=/nifi.sensitive.props.key=this-is-my-sensitive-props-key/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.state.management.embedded.zookeeper.start=false/nifi.state.management.embedded.zookeeper.start=true/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.web.https.host=nifi${n}/nifi.web.https.host=localhost/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED "s/nifi.web.https.port=9443/nifi.web.https.port=944${n}/g" $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
 
   # nifi properties
 
@@ -77,18 +89,18 @@ for n in `seq 1 $NIFI_NODES`; do
     # standard port is 2181; use a non-standard port to avoid conflicts with another zookeeper
     ZK_CONNECT_STRING_VALUE+="localhost:219$m"
   done
-  sed -i '' 's/nifi.zookeeper.connect.string=/nifi.zookeeper.connect.string='"$ZK_CONNECT_STRING_VALUE"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
+  $SED 's/nifi.zookeeper.connect.string=/nifi.zookeeper.connect.string='"$ZK_CONNECT_STRING_VALUE"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/nifi.properties
 
   # zookeeper config
 
   # add lines for each node
   for m in `seq $NIFI_NODES -1 2`; do
     REPLACE_ADD_SERVER_LINE='server.1=\'$'\n''server.'$m'='
-    sed -i '' 's/^server.1=/'"$REPLACE_ADD_SERVER_LINE"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/zookeeper.properties
+    $SED 's/^server.1=/'"$REPLACE_ADD_SERVER_LINE"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/zookeeper.properties
   done
   # set the values for each node
   for m in `seq 1 $NIFI_NODES`; do
-    sed -i '' 's/^server.'"$m"'=/server.'"$m"'=localhost:288'"$m"':388'"$m"';219'"$m"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/zookeeper.properties
+    $SED 's/^server.'"$m"'=/server.'"$m"'=localhost:288'"$m"':388'"$m"';219'"$m"'/g' $NIFI_CLUSTER_DIR/nifi$n/conf/zookeeper.properties
   done
 
   mkdir -p $NIFI_CLUSTER_DIR/nifi$n/state/zookeeper
@@ -103,10 +115,10 @@ for n in `seq 1 $NIFI_NODES`; do
     fi
     ZK_STATE_PROVIDER_VALUE+="localhost:219$m"
   done
-  sed -i '' 's/\>\</\>'"$ZK_STATE_PROVIDER_VALUE"'\</g' $NIFI_CLUSTER_DIR/nifi$n/conf/state-management.xml
+  $SED 's/></>'"$ZK_STATE_PROVIDER_VALUE"'</g' $NIFI_CLUSTER_DIR/nifi$n/conf/state-management.xml
 
   # https://formulae.brew.sh/formula/saxon
-  saxon n=$n -xsl:"$SCRIPT_DIR/authorizers.xslt" -s:"$NIFI_CLUSTER_DIR/nifi$n/conf/authorizers.xml" -o:"$NIFI_CLUSTER_DIR/nifi$n/conf/authorizers.xml"
+  $SAXON n=$n -xsl:"$SCRIPT_DIR/authorizers.xslt" -s:"$NIFI_CLUSTER_DIR/nifi$n/conf/authorizers.xml" -o:"$NIFI_CLUSTER_DIR/nifi$n/conf/authorizers.xml"
 done
 
 # cluster control scripts
