4f3169e8388c81e5ca2f70d92aff8cf0511701c0
Paul Grey <greyp9@yahoo.com>
2022-03-22 16:04:00 -0400

    release verification script

---
+++ release/verify-distributables.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DIST_SOURCE=https://dist.apache.org/repos/dist/dev/nifi/nifi-1.16.0
+DIST_TARGET=~/Downloads/NIFI/1.16.0
+
+BINARIES=(
+  "minifi-1.16.0-bin.tar.gz"
+  "minifi-1.16.0-bin.zip"
+  "minifi-c2-1.16.0-bin.tar.gz"
+  "minifi-c2-1.16.0-bin.zip"
+  "minifi-toolkit-1.16.0-bin.tar.gz"
+  "minifi-toolkit-1.16.0-bin.zip"
+  "nifi-1.16.0-bin.tar.gz"
+  "nifi-1.16.0-bin.zip"
+  "nifi-1.16.0-source-release.zip"
+  "nifi-registry-1.16.0-bin.tar.gz"
+  "nifi-registry-1.16.0-bin.zip"
+  "nifi-registry-toolkit-1.16.0-bin.tar.gz"
+  "nifi-registry-toolkit-1.16.0-bin.zip"
+  "nifi-stateless-1.16.0-bin.tar.gz"
+  "nifi-toolkit-1.16.0-bin.tar.gz"
+  "nifi-toolkit-1.16.0-bin.zip"
+)
+
+EXTENSIONS=(
+  "asc"
+  "sha256"
+  "sha512"
+)
+
+for BINARY in "${BINARIES[@]}"; do
+  echo $BINARY
+  SOURCE=$DIST_SOURCE/$BINARY
+  TARGET=$DIST_TARGET/$BINARY
+  if [[ ! -f $TARGET ]]; then
+    wget -O $TARGET $SOURCE
+  fi
+
+  for EXTENSION in "${EXTENSIONS[@]}"; do
+    SOURCE_X=$SOURCE.$EXTENSION
+    TARGET_X=$TARGET.$EXTENSION
+    if [[ ! -f $TARGET_X ]]; then
+      wget -O $TARGET_X $SOURCE_X
+    fi
+  done
+
+  echo "$(cat $TARGET.sha256) $TARGET" | sha256sum --check
+  echo "$(cat $TARGET.sha512) $TARGET" | sha512sum --check
+  gpg --verify $TARGET.asc
+done
